6cc2661ad675369d0257088c16457bd48313bfb5
FelipeDevFS <felipeolcarvalho1@gmail.com>
2026-03-22 01:07:16 -0300

    Task: Criação de Dashboard e KPIs Iniciais

--- python/app.py
+++ python/app.py
@@ -1,8 +1,28 @@
-import pandas as pd
-from database import get_connection
+import streamlit as st
+from database import get_top_produtos, get_kpis
 
-engine = get_connection()
+st.title("Dashboard Agromercantil")
 
-df = pd.read_sql("SELECT * FROM pedidos", engine)
+# KPIs no topo
+kpis = get_kpis()
 
-print(df.head())
+receita_total = kpis["receita_total"][0]
+total_pedidos = kpis["total_pedidos"][0]
+ticket_medio = kpis["ticket_medio"][0]
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Receita Total", f"R$ {receita_total:.2f}")
+col2.metric("Total de Pedidos", total_pedidos)
+col3.metric("Ticket Médio", f"R$ {ticket_medio:.2f}")
+
+st.divider()
+
+# Top produtos
+st.subheader("Top 5 Produtos Mais Rentáveis")
+
+df = get_top_produtos()
+
+st.bar_chart(df.set_index("nome")["total_vendas"])
+
+st.caption("Tabela detalhada dos produtos")
+st.dataframe(df)
--- python/database.py
+++ python/database.py
@@ -1,5 +1,44 @@
+import pandas as pd
 from sqlalchemy import create_engine
 
 def get_connection():
     engine = create_engine("postgresql://postgres:root@localhost:5432/analise_agro")
     return engine
+
+def get_top_produtos():
+    query = """
+    WITH receita_produtos AS (
+        SELECT
+            ip.id_produto,
+            p.nome,
+            SUM(ip.quantidade * ip.preco_unitario) AS total_vendas
+        FROM itens_pedido ip
+        JOIN pedidos pe ON pe.id_pedido = ip.id_pedido
+        JOIN produtos p ON p.id_produto = ip.id_produto
+        GROUP BY ip.id_produto, p.nome
+    )
+    SELECT 
+        id_produto, 
+        nome, 
+        total_vendas
+    FROM receita_produtos
+    ORDER BY total_vendas DESC
+    LIMIT 5;
+    """
+    
+    engine = get_connection()
+    return pd.read_sql(query, engine)
+
+
+def get_kpis():
+    query = """
+    SELECT 
+        SUM(valor_total) AS receita_total,
+        COUNT(*) AS total_pedidos,
+        AVG(valor_total) AS ticket_medio
+    FROM pedidos;
+    """
+    
+    engine = get_connection()
+    return pd.read_sql(query, engine)
+
